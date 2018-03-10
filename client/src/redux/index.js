f39c0d613ffe4f052b66ebebd3717b0a54da3c7b
chrisperk <christopherperkins.512@gmail.com>
2018-03-09 23:10:56 -0600

    added redux initial state

---
+++ client/src/redux/index.js
@@ -0,0 +1,53 @@
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const initialState = {
+    activeUser: '',
+    searchForm: {
+        searchCriteria: 'streamer',
+        placeholder: 'Enter streamer',
+        isSearching: false,
+        searchText: '',
+        searchResults: [],
+        currentResultsPosition: null,
+        totalResults: null
+    },
+    activeChannelsList: {
+        items: [],
+        activeAudioChannel: null
+    }
+};
+
+const INIT = 'twitchmore/INIT';
+
+const ui = (state = initialState, action) => {
+
+    switch (action.type) {
+
+        case INIT:
+            return {
+                ...state,
+                initialized: true
+            }
+        default:
+            return state;
+
+    }
+
+};
+
+const reducer = combineReducers({
+    ui
+});
+  
+const store = createStore(
+    reducer,
+    composeWithDevTools(
+        applyMiddleware(
+            thunk
+        )
+    )
+);
+  
+export default store;
