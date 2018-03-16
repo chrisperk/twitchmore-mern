95c841cb6d7213a4265ad06eade151e5ea1098f4
chrisperk <christopherperkins.512@gmail.com>
2018-03-16 00:38:51 -0500

    added active channel action creators

--- client/src/redux/activeChannels/index.js
+++ client/src/redux/activeChannels/index.js
@@ -1,5 +1,23 @@
 // Actions
 
-const UNSELECT_STREAM = "UNSELECT_STREAM";
+const UNSELECT_CHANNEL = "UNSELECT_CHANNEL";
 const HIDE_CHANNELLIST = "HIDE_CHANNELLIST";
 const SHOW_CHANNELLIST = "SHOW_CHANNELLIST";
+
+// Action Creators
+
+export const unselectStream = (targetChannel, activeChannels) => ({
+    type: UNSELECT_STREAM,
+    payload: {
+        targetChannel: targetChannel,
+        activeChannels: activeChannels
+    }
+});
+
+export const HIDE_CHANNELLIST = () => ({
+    type: HIDE_CHANNELLIST
+});
+
+export const SHOW_CHANNELLIST = () => ({
+    type: SHOW_CHANNELLIST
+});
