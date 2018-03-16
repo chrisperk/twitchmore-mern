6401cc06f51431cf895215dbc64be4fc992f4abb
chrisperk <christopherperkins.512@gmail.com>
2018-03-16 00:47:59 -0500

    added active channels reducer

--- client/src/redux/activeChannels/index.js
+++ client/src/redux/activeChannels/index.js
@@ -21,3 +21,41 @@ export const HIDE_CHANNELLIST = () => ({
 export const SHOW_CHANNELLIST = () => ({
     type: SHOW_CHANNELLIST
 });
+
+// Reducer
+
+const intialState = {
+    items: [],
+    isShown: false
+};
+
+const reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        
+        case UNSELECT_CHANNEL:
+            return {
+                ...state,
+                items: action.payload.activeChannels.filter(active => active._id !== action.payload.targetChannel._id)
+            };
+
+        case HIDE_CHANNELLIST:
+            return {
+                ...state,
+                isShown: false
+            };
+
+        case SHOW_CHANNELLIST:
+            return {
+                ...state,
+                isShown: true
+            };
+
+        default:
+            return state;
+
+    }
+
+}
+
+export default reducer;
