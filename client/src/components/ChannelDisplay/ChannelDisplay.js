7c7b960c0e1ea4e6b3ef1dc0a3a5298c096dccf1
chrisperk <christopherperkins.512@gmail.com>
2018-03-17 22:24:05 -0500

    channel displays dynamically resize

--- client/src/components/ChannelDisplay/ChannelDisplay.js
+++ client/src/components/ChannelDisplay/ChannelDisplay.js
@@ -1,19 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './ChannelDisplay.css';
 
 const ChannelDisplay = props => {
     return (
-        <div>
-            {props.activeChannels.map(channel => 
-                <iframe
-                    src={`https://player.twitch.tv/?channel=${channel.channel.display_name}&muted=true&autoplay=true&playsinline=true`}
-                    height="800px"
-                    width="600px"
-                    frameBorder="0"
-                    scrolling="no"
-                    allowFullScreen="true"
-                />
-            )}
+        <div id="mobile-display-space">
+            {props.activeChannels.map(channel => {
+                if (props.activeChannels.length === 1) {
+                    return (
+                        <iframe
+                            src={`https://player.twitch.tv/?channel=${channel.channel.display_name}&muted=true&autoplay=true&playsinline=true`}
+                            height="100%"
+                            width="100%"
+                            frameBorder="0"
+                            scrolling="no"
+                            allowFullScreen="true"
+                        />
+                    );
+                }
+                if (props.activeChannels.length === 2) {
+                    return (
+                        <iframe
+                            src={`https://player.twitch.tv/?channel=${channel.channel.display_name}&muted=true&autoplay=true&playsinline=true`}
+                            height="50%"
+                            width="100%"
+                            frameBorder="0"
+                            scrolling="no"
+                            allowFullScreen="true"
+                        />
+                    );
+                } 
+                if (props.activeChannels.length === 3) {
+                    return (
+                        <iframe
+                            src={`https://player.twitch.tv/?channel=${channel.channel.display_name}&muted=true&autoplay=true&playsinline=true`}
+                            height="33.333%"
+                            width="100%"
+                            frameBorder="0"
+                            scrolling="no"
+                            allowFullScreen="true"
+                        />
+                    );
+                }
+                if (props.activeChannels.length === 4) {
+                    return (
+                        <iframe
+                            src={`https://player.twitch.tv/?channel=${channel.channel.display_name}&muted=true&autoplay=true&playsinline=true`}
+                            height="25%"
+                            width="100%"
+                            frameBorder="0"
+                            scrolling="no"
+                            allowFullScreen="true"
+                        />
+                    );
+                }
+            })}
         </div>
     );
 };
