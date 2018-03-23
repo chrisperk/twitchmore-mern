1db89f31617297c40e415c13bfeec5ff4946e509
chrisperk <christopherperkins.512@gmail.com>
2018-03-23 15:02:55 -0500

    added placeholder content when no streams selected

--- client/src/components/ChannelDisplay/ChannelDisplay.js
+++ client/src/components/ChannelDisplay/ChannelDisplay.js
@@ -2,44 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ChannelDisplay.css';
 
+let content;
+
 const ChannelDisplay = props => {
-    return (
-        <div id="mobile-display-space">
-            {props.activeChannels.map(channel => {
-                return (
-                    <div className="channel-wrapper" style={{ height: (() => {
-                        switch (props.activeChannels.length) {
-                            case 1:
-                                return '100%';
-                            case 2:
-                                return '50%';
-                            case 3:
-                                return '33.333%';
-                            case 4:
-                                return '25%';
-                            default:
-                                return '100%';
-                        }
-                    })()}}>
-                        <div className="close-channel-button" onClick={() =>
-                            props.unselectChannel(channel, props.activeChannels)
-                        }>
-                            <i className="fa fa-times" />
+    props.activeChannels.length > 0 ?
+        content = (
+            <div id="mobile-display-space">
+                {props.activeChannels.map(channel => {
+                    return (
+                        <div className="channel-wrapper" style={{ height: (() => {
+                            switch (props.activeChannels.length) {
+                                case 1:
+                                    return '100%';
+                                case 2:
+                                    return '50%';
+                                case 3:
+                                    return '33.333%';
+                                case 4:
+                                    return '25%';
+                                default:
+                                    return '100%';
+                            }
+                        })()}}>
+                            <div className="close-channel-button" onClick={() =>
+                                props.unselectChannel(channel, props.activeChannels)
+                            }>
+                                <span>&times;</span>
+                            </div>
+                            <iframe
+                                src={`https://player.twitch.tv/?channel=${channel.channel ? channel.channel.display_name : channel.display_name}&muted=true&autoplay=true&playsinline=true`}
+                                title={channel.name}
+                                height="100%"
+                                width="100%"
+                                frameBorder="0"
+                                scrolling="no"
+                                allowFullScreen="true"
+                            />
                         </div>
-                        <iframe
-                            src={`https://player.twitch.tv/?channel=${channel.channel ? channel.channel.display_name : channel.display_name}&muted=true&autoplay=true&playsinline=true`}
-                            title={channel.name}
-                            height="100%"
-                            width="100%"
-                            frameBorder="0"
-                            scrolling="no"
-                            allowFullScreen="true"
-                        />
-                    </div>
-                );
-            })}
-        </div>
-    );
+                    );
+                })}
+            </div>
+        ) :
+        content = (
+            <div>Click the search button!</div>
+        );
+    return <div>{content}</div>;
 };
 
 ChannelDisplay.propType = {
